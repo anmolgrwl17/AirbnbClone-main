9e5280c72e1940a36fb5b5c69a3d8e415216a9a7
anmolgrwl17 <anmolgrwl17@gmail.com>
2021-01-14 22:33:02 +0530

    Image fix

---
+++ build/precache-manifest.c358f72443266f82227f3f5542c0c753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6955bb526fa6a343c489b667ca0cb41e",
+    "url": "/AirbnbClone-main/index.html"
+  },
+  {
+    "revision": "c757bb57fb6e73b2975c",
+    "url": "/AirbnbClone-main/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "3ff52b3d6060c272c078",
+    "url": "/AirbnbClone-main/static/css/main.3a0921bc.chunk.css"
+  },
+  {
+    "revision": "c757bb57fb6e73b2975c",
+    "url": "/AirbnbClone-main/static/js/2.63fbc3eb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AirbnbClone-main/static/js/2.63fbc3eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff52b3d6060c272c078",
+    "url": "/AirbnbClone-main/static/js/main.df704c7f.chunk.js"
+  },
+  {
+    "revision": "8c1b905897d6bac50835",
+    "url": "/AirbnbClone-main/static/js/runtime-main.d34a8752.js"
+  }
+]);
